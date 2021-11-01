ea9116bbd4bdeafb13e516e0eaf47420b87ca7fc
= <=>
2021-11-01 20:08:08 +0900

    Initial commit

---
+++ src/test/java/com/starlab/msa/mentalHealth/MentalHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlab.msa.mentalHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
